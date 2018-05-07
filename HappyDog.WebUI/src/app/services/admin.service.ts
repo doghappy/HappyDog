52921e5ecd420ba2226c02a1a9d3ffa2ee0b992d
HeroWong <hero_wong@outlook.com>
2018-05-07 12:07:03 +0800

    part of logic

---
+++ HappyDog.WebUI/src/app/services/admin.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AdminService {
+
+  constructor() { }
+
+  public signIn(userName: string, password: string, rememberMe: boolean): void {
+
+  }
+}
