ae3c026a43cfd04ad042afc67fe71eaf9a57a1d9
HeroWong <hero_wong@outlook.com>
2018-05-18 13:53:38 +0800

    remove sql

--- HappyDog.Api/sql.sql
+++
@@ -1,7 +0,0 @@
-﻿insert into Categories values(1,'#7014e8','.Net',1,'.net');
-insert into Categories values(2,'#f65314','数据库',1,'db');
-insert into Categories values(3,'#00a1f1','Windows',1,'windows');
-insert into Categories values(4,'#7cbb00','阅读',1,'read');
-insert into Categories values(5,'#ffbb00','随笔',1,'essays');
-
-insert into Users values(10000,'000000','HeroWong')
