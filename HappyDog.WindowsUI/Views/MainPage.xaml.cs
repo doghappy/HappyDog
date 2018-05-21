c906d0b2eb40d1f0c767ca87d36135be813033d6
HeroWong <hero_wong@outlook.com>
2018-05-21 15:25:32 +0800

    add uwp proj

---
+++ HappyDog.WindowsUI/Views/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace HappyDog.WindowsUI.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            //SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = ContentFrame.CanGoBack
+            //    ? AppViewBackButtonVisibility.Visible
+            //    : AppViewBackButtonVisibility.Collapsed;
+            //if (e.NavigationMode == NavigationMode.Back)
+            //{
+            //    NavigationViewItem item = null;
+            //    switch (e.SourcePageType)
+            //    {
+            //        case Type t when e.SourcePageType == typeof(HomePage):
+            //        case Type u when e.SourcePageType == typeof(QuestionDetailPage):
+            //            item = NavView.MenuItems[0] as NavigationViewItem;
+            //            break;
+            //        case Type t when e.SourcePageType == typeof(ExplorePage):
+            //            item = NavView.MenuItems[1] as NavigationViewItem;
+            //            break;
+            //        case Type t when e.SourcePageType == typeof(TopicPage):
+            //            item = NavView.MenuItems[2] as NavigationViewItem;
+            //            break;
+            //        case Type t when e.SourcePageType == typeof(MePage):
+            //            item = NavView.MenuItems[3] as NavigationViewItem;
+            //            break;
+            //    }
+            //    item.IsSelected = true;
+            //}
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            //if (args.IsSettingsSelected)
+            //{
+
+            //}
+            //else
+            //{
+            //    var item = args.SelectedItem as NavigationViewItem;
+            //    switch (item.Tag)
+            //    {
+            //        case "Home":
+            //            ContentFrame.Navigate(typeof(HomePage));
+            //            break;
+            //        case "Explore":
+            //            ContentFrame.Navigate(typeof(ExplorePage));
+            //            break;
+            //        case "Topic":
+            //            ContentFrame.Navigate(typeof(TopicPage));
+            //            break;
+            //        case "Me":
+            //            ContentFrame.Navigate(typeof(MePage));
+            //            break;
+            //    }
+            //}
+        }
+    }
+}
