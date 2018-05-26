eb74eae8b5a41687124b9ae9eba9a7d5147f7b66
HeroWong <hero_wong@outlook.com>
2018-05-26 14:35:13 +0800

    封装分类视图代码

--- HappyDog.WindowsUI/Controls/CategoryOutlet.xaml.cs
+++ HappyDog.WindowsUI/Controls/CategoryOutlet.xaml.cs
@@ -1,20 +1,8 @@
-﻿using HappyDog.WindowsUI.Common;
-using HappyDog.WindowsUI.Models;
-using System;
-using System.Collections.Generic;
+﻿using HappyDog.WindowsUI.Models;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace HappyDog.WindowsUI.Controls
 {
@@ -47,16 +35,7 @@ namespace HappyDog.WindowsUI.Controls
             }
         }
 
-        private IncrementalCollection<Article> articles;
-        public IncrementalCollection<Article> Articles
-        {
-            get => articles;
-            set
-            {
-                articles = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Articles)));
-            }
-        }
+        public ObservableCollection<Article> Articles { get; set; }
 
         private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
         {
