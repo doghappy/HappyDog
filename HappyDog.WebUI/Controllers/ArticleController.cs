8b0e2e5b5647b546a3a8cf5416f2bfe298e940f5
HeroWong <hero_wong@outlook.com>
2018-06-28 22:40:58 +0800

    add webui

---
+++ HappyDog.WebUI/Controllers/ArticleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HappyDog.WebUI.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
