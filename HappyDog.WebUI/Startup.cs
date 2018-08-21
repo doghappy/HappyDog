d67286d7124a2084fd9f4a5026ca38d98cc939e5
HeroWong <hero_wong@outlook.com>
2018-08-21 18:39:03 +0800

    fix condition

--- HappyDog.WebUI/Startup.cs
+++ HappyDog.WebUI/Startup.cs
@@ -78,7 +78,7 @@ namespace HappyDog.WebUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (!env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
