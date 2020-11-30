de2ff939d9ddc877b893f5672b49d322e2ec83a4
HeroWong <hero_wong@outlook.com>
2020-11-30 20:22:49 +0800

    feat: /windows

--- HappyDog.Java/src/main/java/wang/doghappy/java/module/article/ArticleController.java
+++ HappyDog.Java/src/main/java/wang/doghappy/java/module/article/ArticleController.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import wang.doghappy.java.module.article.template.DatabaseTemplate;
-import wang.doghappy.java.module.article.template.IndexTemplate;
-import wang.doghappy.java.module.article.template.JavaTemplate;
-import wang.doghappy.java.module.article.template.NetTemplate;
+import wang.doghappy.java.module.article.template.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -70,5 +67,16 @@ public class ArticleController {
         var template = new DatabaseTemplate();
         template.setData(request, model, page, articleService);
         return "article/database";
+    }
+
+    @GetMapping("/windows")
+    public String windows(
+            Model model,
+            HttpServletRequest request,
+            @RequestParam(defaultValue = "1") int page
+    ) {
+        var template = new WindowsTemplate();
+        template.setData(request, model, page, articleService);
+        return "article/windows";
     }
 }
