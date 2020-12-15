b9eab884525c85de649835398b3b39e5ea706476
HeroWong <hero_wong@outlook.com>
2020-12-15 15:17:18 +0800

    feat: remember me

--- HappyDog.Java/src/main/java/wang/doghappy/java/config/SecurityConfig.java
+++ HappyDog.Java/src/main/java/wang/doghappy/java/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureHandler(new LoginFailureHandler())
                 .permitAll()
                 .and()
+                .rememberMe()
+                .and()
                 .logout().permitAll();
     }
 
